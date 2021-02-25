58e2e42182828870cf8bd5dbe835de8e2b32d9c7
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-02-25 22:32:48 +0000

    Java Exercise 8.5 Part 4 Completed

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //a) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
-
-
+console.log("*****CONCAT*****");
+newArray = holdCabinet1.concat(holdCabinet2);
+console.log(newArray);
 
 //b) Print a slice of two elements from each array. Does slice alter the original arrays?
-
-
+console.log("*****SLICE*****");
+holdCabinet1.slice(2);
+console.log(holdCabinet1);
+holdCabinet2.slice(2);
+console.log(holdCabinet2);
 
 //c) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+console.log("*****REVERSE*****");
+holdCabinet1.reverse();
+console.log(holdCabinet1);
+console.log("*****SORT*****");
+holdCabinet2.sort();
+console.log(holdCabinet2);
